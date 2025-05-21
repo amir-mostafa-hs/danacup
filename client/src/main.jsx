18bb10a6b167ee2902ab1cf20c96bf130e4e3e47
Amir Mostafa Haji Sadeghian <amir-mostafa-hs@outlook.com>
2025-05-21 08:40:13 +0330

    remove .env in ignore

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,21 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import App from "./App.jsx";
 
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </StrictMode>
 );
